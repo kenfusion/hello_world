5a4aa029ab13ca6f286aff5331b935b0d73a0f78
Chris Malone <surefoot@gmail.com>
2016-09-06 09:10:27 -0400

    Enable autosign.  Change java memory to 512m.

--- puppet/puppetserver-install.sh
+++ puppet/puppetserver-install.sh
@@ -2,8 +2,13 @@
 set -e
 
 sudo yum install -y puppetserver
-sudo service puppetserver start
-sudo chkconfig puppetserver on
+
+# change java memory allocation from 2g to 512m
+# https://docs.puppet.com/puppetserver/latest/install_from_packages.html#memory-allocation
+sudo sed -i 's/2g/512m/g' /etc/sysconfig/puppetserver
+
+# enable cert autosign
+sudo sh -c 'echo "*.example.com" >> /etc/puppetlabs/puppet/autosign.conf'
 
 sudo ln -s /vagrant/puppet/manifests/site.pp /etc/puppetlabs/code/environments/production/manifests/site.pp
 sudo rm -rf /etc/puppetlabs/code/environments/production/modules/
@@ -15,3 +20,5 @@ sudo /opt/puppetlabs/bin/puppet module install puppetlabs-mysql
 # sudo /opt/puppetlabs/bin/puppet module install puppetlabs-haproxy
 # sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apache
 
+sudo service puppetserver start
+sudo chkconfig puppetserver on
