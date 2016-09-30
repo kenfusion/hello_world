7a04c309d282c01ff8759beb164967070b522d2e
Chris Malone <surefoot@gmail.com>
2016-09-30 07:00:55 -0400

    Move example-roles to roles

--- ansible/provision_stack.sh
+++ ansible/provision_stack.sh
@@ -5,4 +5,4 @@ set -e
 ansible all -m setup
 
 # run the site playbook
-ansible-playbook /home/vagrant/ansible/example-roles/site.yml --vault-password-file /home/vagrant/ansible/super_sekrit_file
+ansible-playbook /home/vagrant/ansible/roles/site.yml --vault-password-file /home/vagrant/ansible/super_sekrit_file
